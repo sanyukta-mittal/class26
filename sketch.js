3c4032dcd267e3148a1a6916df5fc496199a59d0
sanyukta-mittal <sanyuktamittal1706@gmail.com>
2021-01-18 18:58:49 +0530

    deleted the platform obj. and added the bird obj.

--- sketch.js
+++ sketch.js
@@ -17,7 +17,8 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+  
+    pig4 = new Pig(70,80,100,90)
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
